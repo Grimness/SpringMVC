b801b193a9566aacd5bb2910b5e70262183421bc
liang hy <javac@lilianghy.top>
2016-05-14 19:40:25 +0800

    一个IOC容器的小项目

---
+++ springDemo/src/com/lilianghy/demo3_ioc/App2.java
@@ -0,0 +1,23 @@
+package com.lilianghy.demo3_ioc;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App2 {
+	private ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/lilianghy/demo3_ioc/bean2.xml");
+
+	@Test
+	public void SpringIocDemo(){
+		
+		//加载Spring配置文件  applicationContext.xml
+//       System.out.println(applicationContext.getBean("user"));
+//       System.out.println(applicationContext.getBean("str"));
+			System.out.println(applicationContext.getBean("user2"));
+        
+	}
+	
+	
+	
+	
+}
